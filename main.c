9d052e2e795bcf40df4855035977c24c3d6ecfc4
Jeon-Ji-Woo <jiwoo3514@sookmyung.ac.kr>
2023-11-09 15:53:24 +0900

    실습5

--- main.c
+++ main.c
@@ -5,10 +5,18 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int i=0;
-	char src[] ="happy C programming";
+	FILE* fp;
+	char str[100];
+	int i;
+	fp = fopen("sample.txt", "w");
 	
-	printf("문자열 \" %s\"의 길이 : %i\n", src, strlen(src) );
+	for (i=0; i<3;i++)
+	{
+		printf("input a word : ");
+		scanf("%s", str);
+		fprintf(fp, "%s\n", str);
+	}
+	fclose(fp);
 	
 	system("PAUSE");
 	return 0;
